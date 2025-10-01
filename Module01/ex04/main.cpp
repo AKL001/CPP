81c01b3588fe5b30d2f4b565c4af03b42daf7be7
AKL001 <l9udyr@gmail.com>
2025-10-01 15:55:35 +0100

    ex04 start

---
+++ Module01/ex04/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+/*  edge  cases :
+    - if S1 empty;
+    - if S1 == S2; 
+    - if more than 3 agruments
+    - if we can open the file permesion , if the file exist
+*/
+
+/*
+    File handling:
+        std::ifstream -> readfile.
+        std::ofstream -> writeFile.
+        getline(<filename> , line) -> read line by line
+    
+    String manipulation:
+        std::string::find(S1, pos) -> ???
+        std::string::append  or (+=) 
+        std::sting::length() or (.size())
+         
+*/
+
+int main(int ac,char **av)
+{
+
+
+
+
+
+}
